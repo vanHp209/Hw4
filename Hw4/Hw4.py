77a205fe17af556bc4ff37231f719c53b90d73d1
vanHp209 <van.borysov@gmail.com>
2024-10-28 16:10:40 +0200

    Add files via upload

---
+++ Hw4/Hw4.py
@@ -0,0 +1,11 @@
+class squar:
+    def __init__(self, height=160):  # параметр по умолчанию
+        self.Width = (6)
+        self.Leng = (9)
+    def calculate_perimetr(self,Width =20,Leng=5):
+        print(self.Width + self.Leng + self.Width + self.Leng)
+    def calculate_area(self,Width =20,Leng=5):
+        print(self.Width * self.Leng)
+
+Width = squar('milk')
+Leng = squar(20)
